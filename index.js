a932bd8837d9501b4aff212c5a6fd0b931e23632
Viole Grace <highdevilxx@hotmail.com>
2018-08-13 20:22:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function removeFromCart(item)
       }
       if (!itemInCart)
       {
-        return("That item is not in your cart.");
+        return "That item is not in your cart.";
       }
 
     }
