a4b3baaff4f06aa57e87f70724b09ca4cc871af8
Viole Grace <highdevilxx@hotmail.com>
2018-08-13 20:30:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -86,7 +86,8 @@ function placeOrder(cardNumber)
     return ("Sorry, we don't have a credit card on file for you.");
   } else
   {
+    var totalpricy=total();
     cart = [];
-    return ("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + "." );
+    return ("Your total cost is $" +  + ", which will be charged to the card " + cardNumber + "." );
   }
 }
